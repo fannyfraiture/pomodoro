a56a7c11aa618a1444002380a518fc18450f08db
Fanny Fraiture <ffraiture.fanny@gmail.com>
2021-11-19 15:17:31 +0100

    mauvaise version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './app/App'
-import './main.scss'
+import App from './componnents/App.js'
+
 
 ReactDOM.render(
  <App />,
